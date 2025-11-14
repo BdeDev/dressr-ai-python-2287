fef37ee22887e380f5ddc99d756400314d8d3e38
Prity Kumari <prity.kumari@toxsltech.com>
2025-11-14 13:01:48 +0000

    Worked on client feedback points

--- app/contact_us/urls.py
+++ app/contact_us/urls.py
@@ -13,7 +13,11 @@ urlpatterns = [
     re_path(r'view-contact-us-details/(?P<id>[-\w]+)/$',ViewContactUsDetails.as_view(),name="view_contact"),
     re_path(r'delete-contact-us/(?P<id>[-\w]+)/$',DeleteContactUs.as_view(),name="delete_contact"),
     re_path(r'contactus-reply/$',ContactUsReplyView.as_view(),name="contactus_reply"),
+    re_path(r'contact-us-request/$',ContactUsView.as_view(),name="contact_us_request"),
+    re_path(r'clear-admin-details/$',ClearAdminDetails.as_view(),name="clear_admin_details"),
 
-    ## Social Links Management
-    re_path(r'social-links/$',SocialLinksView.as_view(),name="social_links"),
+    ## Contact Details
+    re_path(r'update-contact-details/$',UpdateContactDetails.as_view(),name="update_contact_details"),
+    re_path(r'update-social-links/$',UpdateSocialLinks.as_view(),name="social_links"),
+    re_path(r'clear-social-links/$',ClearSocialLinks.as_view(),name="clear_social_links"),
 ]
